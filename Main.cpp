a72ababf073e2262115aae186ae673819f7bce03
John Roberts <jroberts@student.neumont.edu>
2022-12-01 16:59:31 -0700

    Caught up ray trace

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,10 @@ int main(int, char**)
 {
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(400, 300);
+	renderer.CreateWindow(800, 600);
+
+	Canvas canvas(800, 600, renderer);	
+
 	bool quit = false;
 	while (!quit)
 	{
@@ -15,7 +18,26 @@ int main(int, char**)
 		case SDL_QUIT:
 			quit = true;
 			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
 		}
+
+		//render scene
+		canvas.Clear({0, 0, 0, 1});
+		for (int i = 0; i < 10000; i++) {
+
+			canvas.DrawPoint({ random(0, 800), random(0, 600)}, {1, 1, 1, 1});
+		}
+		canvas.Update();
+
+		renderer.CopyCanvas(canvas);
+		renderer.Present();
 	}
 	renderer.Shutdown();
 	return 0;
--- Renderer/Renderer.h
+++ Renderer/Renderer.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "Canvas.h"
 #include <SDL.h>
+
 class Renderer
 {
 public:
@@ -7,6 +9,12 @@ public:
 	bool Initialize();
 	void Shutdown();
 	bool CreateWindow(int width, int height);
+
+	void CopyCanvas(const Canvas& canvas);
+	void Present();
+
+	friend class Canvas;
+
 private:
 	SDL_Window* m_window{ nullptr };
 	SDL_Renderer* m_renderer{ nullptr };
